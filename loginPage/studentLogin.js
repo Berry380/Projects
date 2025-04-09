66ea178f1df055cbc6208bdd918910343e8b2416
Berry380 <hatterberry29@gmail.com>
2025-04-09 16:59:14 -0500

    Delete loginPage directory

--- loginPage/studentLogin.js
+++
@@ -1,7 +0,0 @@
-import { db } from '../../firebase/init.js';
-import { collection, addDoc, getDocs, doc, setDoc, deleteDoc } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js';
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
-
-outputDiv.addEventListener('click', async (event) => {
-    
-});
